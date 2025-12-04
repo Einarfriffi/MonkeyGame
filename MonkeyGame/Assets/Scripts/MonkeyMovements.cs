64b68f98afc356ac90456d9b2c91216f6c68c626
reginasjofn <reginasjofn@gmail.com>
2025-12-04 13:09:36 +0000

    Double Jump
    Now he can only jump twice without touching the ground in between

--- MonkeyGame/Assets/Scripts/MonkeyMovements.cs
+++ MonkeyGame/Assets/Scripts/MonkeyMovements.cs
@@ -6,17 +6,25 @@ public class MonkeyMovements : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
 
+    [Header("Jumps")]
+    [SerializeField] private int maxJumps = 2; 
+
     private GroundController _groundController;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _moveInput;
     private event System.Action _jumpPressed;
     private bool _jumpTriggered;
 
+    private int _jumpsRemaining;
+    private bool _wasGrounded;
+
     private void Start()
     {
         _groundController = GetComponent<GroundController>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _jumpPressed += JumpButtonPressed;
+
+        _jumpsRemaining = maxJumps;
     }
 
     private void OnDestroy()
@@ -26,12 +34,22 @@ public class MonkeyMovements : MonoBehaviour
 
     private void Update()
     {
+        bool isGrounded = _groundController != null && _groundController.IsGrounded;
+
+        if (isGrounded && !_wasGrounded)
+        {
+            _jumpsRemaining = maxJumps;
+        }
+
+        _wasGrounded = isGrounded;
+
         Vector2 velocity = _rigidbody2D.linearVelocity;
         velocity.x = _moveInput.x * speed;
 
-        if (_jumpTriggered)
+        if (_jumpTriggered && _jumpsRemaining > 0)
         {
             velocity.y = jumpForce;
+            _jumpsRemaining--;
             _jumpTriggered = false;
         }
 
@@ -53,7 +71,7 @@ public class MonkeyMovements : MonoBehaviour
 
     private void JumpButtonPressed()
     {
-        if (_groundController != null && _groundController.IsGrounded)
+        if (_jumpsRemaining > 0)
         {
             _jumpTriggered = true;
         }
