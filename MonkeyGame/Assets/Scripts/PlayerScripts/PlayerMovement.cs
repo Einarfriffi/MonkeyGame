03b70df34616a2e2a55a5984753181ae99952cf8
steini-jonsson-mac <steinijons@outlook.com>
2025-12-11 21:30:48 +0000

    UI for Tutotial Level, Music stops in pause

--- MonkeyGame/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ MonkeyGame/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -328,6 +328,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void AimAtMouse()
     {
+        if (Time.timeScale == 0) return;
         Vector3 mouseScreenPos = Mouse.current.position.ReadValue();
         Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos);
 
--- MonkeyGame/Assets/Scripts/UIScripts/PauseMenu.cs
+++ MonkeyGame/Assets/Scripts/UIScripts/PauseMenu.cs
@@ -10,23 +10,27 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        MusicManager.Instance.PauseMusic();
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        MusicManager.Instance.ResumeMusic();
     }
 
     public void Home()
     {
         SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1;
+        MusicManager.Instance.ResumeMusic();
     }
 
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+        MusicManager.Instance.ResumeMusic();
     }
 }
