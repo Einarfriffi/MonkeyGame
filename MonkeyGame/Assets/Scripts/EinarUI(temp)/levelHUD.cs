e1e0129d9a09bcc872a3334210ddc20ba420a8c1
Einar <Einarfriffi@gmail.com>
2025-12-12 13:45:21 +0000

    minor playtest fixes

--- MonkeyGame/Assets/Scripts/EinarUI(temp)/levelHUD.cs
+++ MonkeyGame/Assets/Scripts/EinarUI(temp)/levelHUD.cs
@@ -34,7 +34,8 @@ public class levelHUD : MonoBehaviour
         int minutes = Mathf.FloorToInt(time / 60f);
         int seconds = Mathf.FloorToInt(time % 60f);
         int milliseconds = Mathf.FloorToInt((time * 100f) % 100f);
-        return $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+        return $"{minutes:00}:{seconds:00}:{milliseconds:00}".PadLeft(8, '0');
+        // return $"{minutes:00}:{seconds:00}:{milliseconds:00}";
     }
 
     // Set Level text for component
